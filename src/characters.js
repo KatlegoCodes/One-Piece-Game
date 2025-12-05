c07155407a49e74decc6e02f42767d3ff0b1ca44
Frost <moosemashegona@gmail.com>
2025-12-05 10:33:50 +0200

    Added new characters to the roster

--- src/characters.js
+++ src/characters.js
@@ -40,6 +40,66 @@ import izu_image from "./assets/izu.jpg";
 import tashigi_image from "./assets/tashigi.jpg";
 import pudding_image from "./assets/pudding.jpg";
 import queen_image from "./assets/queen.jpg";
+import barthelemeo from "./assets/barthelemeo.jpg";
+import cavendish from "./assets/cavandish.jpg";
+import cracker from "./assets/cracker.jpg"
+import kalifa from "./assets/khalifa.jpg"
+import whos_who from "./assets/whoswho.jpg"
+import vivi from "./assets/vivi.jpg"
+import carrot from "./assets/carrot.jpg"
+import yamato from "./assets/yamato.jpg"
+import kid from "./assets/eustass kid.jpg";
+import killer from "./assets/killer.jpg";
+import urouge from "./assets/urouge.jpg";
+import bonney from "./assets/bonney.jpg";
+import hawkins from "./assets/hawkings.jpg";
+import sakazuki from "./assets/sakazuki.jpg";
+import bege from "./assets/bege.jpg";
+import drake from "./assets/drake.jpg";
+import issho from "./assets/issho.jpg";
+import wiper from "./assets/wiper.jpg";
+import garp from "./assets/garp.jpg";
+import smoker from "./assets/smoker.jpg";
+import hina from "./assets/hinah.jpg";
+import greenbull from "./assets/greenbull.jpg";
+import dragon from "./assets/dragon.jpg";
+import sabo from "./assets/sabo.jpg";
+import belo from "./assets/bella betty.jpg";
+import lindbergh from "./assets/lindbergh.jpg";
+import karasu from "./assets/karasu.jpg";
+import kinemon from "./assets/kenimon.jpg";
+import ashura from "./assets/ashura.jpg";
+import denjiro from "./assets/denjiro.jpg";
+import hiyori from "./assets/hiyori.jpg";
+import kawamatsu from "./assets/kawamatsu.jpg";
+import shinobu from "./assets/shinobou.jpg";
+import buggy from "./assets/buggy.jpg";
+import kuma from "./assets/kuma.jpg";
+import moria from "./assets/moria.jpg";
+import weevil from "./assets/weevil.jpg";
+import bon_clay from "./assets/bonclay.jpg";
+import foxy from "./assets/foxy.jpg";
+import perona from "./assets/perona.jpg";
+import enel from "./assets/enel.jpg";
+import shiryu from "./assets/shiryu.jpg";
+import alvida from "./assets/alvida.jpg";
+import ben from "./assets/ben beckman.jpg";
+import baby_5 from "./assets/baby 5.jpg";
+import pizzaro from "./assets/pizzaro.jpg";
+import shaky from "./assets/shakky.jpg";
+import vista from "./assets/vista.jpg";
+import sengoku from "./assets/sengoku.jpg";
+import rayleigh from "./assets/rayleigh.jpg";
+import raizo from "./assets/raizo.jpg";
+import corazon from "./assets/corazon.jpg";
+import black_maria from "./assets/black maria.jpg";
+
+
+
+
+
+
+
 
 export const characters = [
   {
@@ -590,4 +650,734 @@ export const characters = [
       "I'm something of a scientist.",
     ],
   },
+
+  // ---------------------- NEW CHARACTER ENTRIES ----------------------
+
+{
+  name: "Bartolomeo",
+  alias: ["bartolomeo", "cannibal"],
+  image: barthelemeo,
+  crew: "Barto Club",
+  role: "Captain",
+  fruit: "Bari Bari no Mi",
+  hints: [
+    "I'm one of Luffy's biggest fans.",
+    "I ate the Bari Bari no Mi.",
+    "I lead the Barto Club.",
+  ],
+},
+{
+  name: "Cavendish",
+  alias: ["cavendish", "prince pirate", "cabage"],
+  image: cavendish,
+  crew: "Beautiful Pirates",
+  role: "Captain",
+  fruit: "None",
+  hints: [
+    "I'm known for my beauty.",
+    "I have another personality called Hakuba.",
+    "I'm part of the Straw Hat Grand Fleet.",
+  ],
+},
+{
+  name: "Who's-who",
+  alias: ["who's who", "who's-who"],
+  image: whos_who,
+  crew: "Tobbi Roppo",
+  role: "Member",
+  fruit: "neko neko no Mi, Model: Saber Tiger",
+  hints: [
+    "I'm former member of cipher pol",
+    "I'm a member of Tobi Roppo",
+    "I at the neko neko no mi",
+  ],
+},
+{
+  name: "Shakky",
+  alias: ["Shakky", "shakuyaku"],
+  image: shaky,
+  crew: "Kuja Pirates",
+  role: "Captain",
+  fruit: "None",
+  hints: [
+    "I'm a former member of the Kuja Pirates.",
+    "I'm a former Empress of Amazon Lilly.",
+    "I was the main prize at god valley.",
+  ],
+},
+{
+  name: "Okiku",
+  alias: ["kiku", "Okiku"],
+  image: kiku_image,
+  crew: "Wano Samurai",
+  role: "Retainer",
+  fruit: "None",
+  hints: [
+    "I'm a former retainer of Oden .",
+    "I dream of reviving the Giant Warrior Pirates.",
+    "I joined the Straw Hat Grand Fleet.",
+  ],
+},
+{
+  name: "Kalifa",
+  alias: ["Kalifa"],
+  image: kalifa,
+  crew: "CP-9",
+  role: "Secret service",
+  fruit: "Awa Awa no Mi",
+  hints: [
+    "I'm a former member of CP-9.",
+    "I specialize in inflitration.",
+    "I'm a former personal assistant of iceberg.",
+  ],
+},
+{
+  name: "Charlotte Cracker",
+  alias: ["cracker"],
+  image: cracker,
+  crew: "Big Mom Pirates",
+  role: "Sweet Commander",
+  fruit: "Bisu Bisu no Mi",
+  hints: [
+    "I'm one of the sweet commanders.",
+    "I'm a biscuit man.",
+    "I'm the son of a Yonkou.",
+  ],
+},
+{
+  name: "Nefertari Vivi",
+  alias: ["vivi"],
+  image: vivi,
+  crew: "Alabasta Kingdom",
+  role: "Princess",
+  fruit: "None",
+  hints: [
+    "I'm the princess of the Alabasta Kingdom.",
+    "I once traveled with the Straw Hats.",
+    "I'm beloved by my people.",
+  ],
+},
+{
+  name: "Carrot",
+  alias: ["carrot"],
+  image: carrot,
+  crew: "Mink Tribe",
+  role: "Warrior",
+  fruit: "None",
+  hints: [
+    "I'm a mink from Zou.",
+    "I have a special form during the full moon.",
+    "I traveled with the Straw Hats.",
+  ],
+},
+{
+  name: "Yamato",
+  alias: ["yamato", "kaido's son"],
+  image: yamato,
+  crew: "None",
+  role: "Samurai",
+  fruit: "Inu Inu no Mi, Model: Okuchi no Makami",
+  hints: [
+    "I admire Kozuki Oden.",
+    "I'm the child of Kaidou.",
+    "I can transform into a mythical wolf.",
+  ],
+},
+
+// ---- Worst Generation ----
+
+{
+  name: "Eustass Kid",
+  alias: ["kid", "eustass kid"],
+  image: kid,
+  crew: "Kid Pirates",
+  role: "Captain",
+  fruit: "Jiki Jiki no Mi",
+  hints: [
+    "I'm the captain of the Kid Pirates.",
+    "I manipulate metal using magnetism.",
+    "I'm part of the Worst Generation.",
+  ],
+},
+{
+  name: "Killer",
+  alias: ["killer", "kamazo"],
+  image: killer,
+  crew: "Kid Pirates",
+  role: "Combatant",
+  fruit: "None",
+  hints: [
+    "I'm the right-hand man of Eustass Kid.",
+    "I wear a mask.",
+    "I'm part of the Worst Generation.",
+  ],
+},
+{
+  name: "Basil Hawkins",
+  alias: ["hawkins", "magician"],
+  image: hawkins,
+  crew: "Hawkins Pirates",
+  role: "Captain",
+  fruit: "Wara Wara no Mi",
+  hints: [
+    "I use tarot cards in battle.",
+    "I can redirect damage to straw dolls.",
+    "I'm part of the Worst Generation.",
+  ],
+},
+{
+  name: "X Drake",
+  alias: ["drake", "x drake"],
+  image: drake,
+  crew: "Drake Pirates",
+  role: "Captain",
+  fruit: "Ryu Ryu no Mi, Model: Allosaurus",
+  hints: [
+    "I can transform into a dinosaur.",
+    "I was secretly a marine.",
+    "I'm part of the Worst Generation.",
+  ],
+},
+{
+  name: "Jewelry Bonney",
+  alias: ["bonney"],
+  image: bonney,
+  crew: "Bonney Pirates",
+  role: "Captain",
+  fruit: "Toshi Toshi no Mi",
+  hints: [
+    "I can change people's ages.",
+    "I'm part of the Worst Generation.",
+    "I have a big appetite.",
+  ],
+},
+{
+  name: "Urouge",
+  alias: ["urouge", "mad monk"],
+  image: urouge,
+  crew: "Urouge Pirates",
+  role: "Captain",
+  fruit: "None",
+  hints: [
+    "I'm a monk.",
+    "The more damage I take, the stronger I get.",
+    "I'm part of the Worst Generation.",
+  ],
+},
+{
+  name: "Capone Bege",
+  alias: ["bege", "capone"],
+  image: bege,
+  crew: "Fire Tank Pirates",
+  role: "Captain",
+  fruit: "Shiro Shiro no Mi",
+  hints: [
+    "My body works like a fortress.",
+    "I'm part of the Worst Generation.",
+    "I helped save Sanji during the wedding.",
+  ],
+},
+{
+  name: "Vista",
+  alias: ["Vista"],
+  image: vista,
+  crew: "Whitebeard pirates",
+  role: "5th Captain",
+  fruit: "None",
+  hints: [
+    "I'm a swordsman of the whitebeard Pirates",
+    "I'm an enemy of yassop",
+    "I'm on par with mihawk",
+  ],
+},
+
+// ---- Marines ----
+
+{
+  name: "Sakazuki",
+  alias: ["akainu", "sakazuki"],
+  image: sakazuki,
+  crew: "Marines",
+  role: "Fleet Admiral",
+  fruit: "Magu Magu no Mi",
+  hints: [
+    "I believe in absolute justice.",
+    "I killed Ace.",
+    "I command the Marines.",
+  ],
+},
+{
+  name: "Issho",
+  alias: ["fujitora", "issho"],
+  image: issho,
+  crew: "Marines",
+  role: "Admiral",
+  fruit: "Zushi Zushi no Mi",
+  hints: [
+    "I'm blind.",
+    "I can control gravity.",
+    "I'm one of the Marine admirals.",
+  ],
+},
+{
+  name: "Aramaki",
+  alias: ["greenbull", "aramaki"],
+  image: greenbull,
+  crew: "Marines",
+  role: "Admiral",
+  fruit: "Mori Mori no Mi",
+  hints: [
+    "I can manipulate plants.",
+    "I hadn't eaten for years.",
+    "I'm one of the Marine admirals.",
+  ],
+},
+{
+  name: "Smoker",
+  alias: ["smoker", "white chase"],
+  image: smoker,
+  crew: "Marines",
+  role: "Vice Admiral",
+  fruit: "Moku Moku no Mi",
+  hints: [
+    "I use two jitte weapons.",
+    "I chase pirates across the seas.",
+    "I can turn into smoke.",
+  ],
+},
+{
+  name: "Hina",
+  alias: ["hina"],
+  image: hina,
+  crew: "Marines",
+  role: "Commodore",
+  fruit: "Ori Ori no Mi",
+  hints: [
+    "I can trap enemies using my iron cage ability.",
+    "I'm a high-ranking Marine.",
+    "I'm friends with Smoker.",
+  ],
+},
+{
+  name: "Sengoku",
+  alias: ["sengoku", "buddha sengoku"],
+  image: sengoku,
+  crew: "Marines",
+  role: "Former Fleet Admiral",
+  fruit: "Hito Hito no Mi, Model: Daibutsu",
+  hints: [
+    "I can transform into a giant Buddha.",
+    "I'm a former Fleet Admiral.",
+    "I fought in the Marineford War.",
+  ],
+},
+{
+  name: "Monkey D. Garp",
+  alias: ["garp", "hero of the marines"],
+  image: garp,
+  crew: "Marines",
+  role: "Vice Admiral",
+  fruit: "None",
+  hints: [
+    "I'm the hero of the Marines.",
+    "I'm Luffy's grandfather.",
+    "I rely on overwhelming physical strength.",
+  ],
+},
+{
+  name: "Wiper",
+  alias: ["wiper"],
+  image: wiper,
+  crew: "Shandorians",
+  role: "Warrior",
+  fruit: "None",
+  hints: [
+    "I am a warrior of Shandora",
+    "I'm a resident of Skypea",
+    "I'm an enemy of Enel",
+  ],
+},
+
+// ---- Revolutionary Army ----
+
+{
+  name: "Monkey D. Dragon",
+  alias: ["dragon"],
+  image: dragon,
+  crew: "Revolutionary Army",
+  role: "Supreme Commander",
+  fruit: "None",
+  hints: [
+    "I'm the father of Luffy.",
+    "I'm the world's most wanted man.",
+    "I lead the Revolutionary Army.",
+  ],
+},
+{
+  name: "Sabo",
+  alias: ["sabo"],
+  image: sabo,
+  crew: "Revolutionary Army",
+  role: "Chief of Staff",
+  fruit: "Mera Mera no Mi",
+  hints: [
+    "I'm Luffy's sworn brother.",
+    "I ate the Mera Mera no Mi.",
+    "I'm second-in-command of the Revolutionary Army.",
+  ],
+},
+{
+  name: "Belo Betty",
+  alias: ["belo betty"],
+  image: belo,
+  crew: "Revolutionary Army",
+  role: "East Army Commander",
+  fruit: "Kobu Kobu no Mi",
+  hints: [
+    "I inspire courage in others.",
+    "I'm a commander in the Revolutionary Army.",
+    "I wield a flag as a weapon.",
+  ],
+},
+{
+  name: "Lindbergh",
+  alias: ["lindbergh"],
+  image: lindbergh,
+  crew: "Revolutionary Army",
+  role: "South Army Commander",
+  fruit: "None",
+  hints: [
+    "I'm a mink.",
+    "I specialize in inventions.",
+    "I'm a commander in the Revolutionary Army.",
+  ],
+},
+{
+  name: "Karasu",
+  alias: ["karasu"],
+  image: karasu,
+  crew: "Revolutionary Army",
+  role: "North Army Commander",
+  fruit: "Suspected Crow Fruit",
+  hints: [
+    "I control crows.",
+    "I always cover my mouth.",
+    "I'm a commander in the Revolutionary Army.",
+  ],
+},
+
+// ---- Wano Characters ----
+
+{
+  name: "Rayleigh",
+  alias: ["Rayleigh", "Dark King"],
+  image: rayleigh,
+  crew: "Roger Pirates",
+  role: "Right Hand",
+  fruit: "None",
+  hints: [
+    "I'm the former right hand of the pirate king",
+    "I'm the lover of Shakky",
+    "I'm known as the dark king",
+  ],
+},
+{
+  name: "Kin'emon",
+  alias: ["kinemon"],
+  image: kinemon,
+  crew: "Nine Red Scabbards",
+  role: "Leader",
+  fruit: "Fuku Fuku no Mi",
+  hints: [
+    "I'm a samurai of Wano.",
+    "I can create outfits using my Devil Fruit.",
+    "I'm one of Oden's retainers.",
+  ],
+},
+{
+  name: "Denjiro",
+  alias: ["denjiro", "kyoshiro"],
+  image: denjiro,
+  crew: "Nine Red Scabbards",
+  role: "Retainer",
+  fruit: "None",
+  hints: [
+    "I once assumed the identity of Kyoshiro.",
+    "I'm a master swordsman.",
+    "I'm one of Oden's retainers.",
+  ],
+},
+{
+  name: "Ashura Doji",
+  alias: ["ashura"],
+  image: ashura,
+  crew: "Nine Red Scabbards",
+  role: "Retainer",
+  fruit: "None",
+  hints: [
+    "I was once a bandit.",
+    "I'm extremely strong for a human.",
+    "I'm one of Oden's retainers.",
+  ],
+},
+{
+  name: "Raizo",
+  alias: ["raizo"],
+  image: raizo,
+  crew: "Nine Red Scabbards",
+  role: "Ninja",
+  fruit: "Maki Maki no Mi",
+  hints: [
+    "I'm a ninja from Wano.",
+    "I can create scroll-based attacks.",
+    "I'm one of Oden's retainers.",
+  ],
+},
+{
+  name: "Kawamatsu",
+  alias: ["kawamatsu"],
+  image: kawamatsu,
+  crew: "Nine Red Scabbards",
+  role: "Retainer",
+  fruit: "None",
+  hints: [
+    "I'm a kappa-like samurai.",
+    "I'm one of Oden's retainers.",
+    "I can swim exceptionally well.",
+  ],
+},
+{
+  name: "Hiyori",
+  alias: ["hiyori", "komurasaki"],
+  image: hiyori,
+  crew: "Wano Country",
+  role: "Princess",
+  fruit: "None",
+  hints: [
+    "I'm the daughter of Oden.",
+    "I once disguised myself as Komurasaki.",
+    "I play the shamisen beautifully.",
+  ],
+},
+{
+  name: "Shinobu",
+  alias: ["shinobu"],
+  image: shinobu,
+  crew: "Wano",
+  role: "Kunoichi",
+  fruit: "Juku Juku no Mi",
+  hints: [
+    "I'm a kunoichi.",
+    "I can age things rapidly.",
+    "I served Oden faithfully.",
+  ],
+},
+
+// ---- Warlords ----
+
+{
+  name: "Buggy",
+  alias: ["buggy", "buggy the clown"],
+  image: buggy,
+  crew: "Buggy Pirates",
+  role: "Captain",
+  fruit: "Bara Bara no Mi",
+  hints: [
+    "I can split my body into pieces.",
+    "I was part of Roger’s crew.",
+    "I'm hilariously overhyped.",
+  ],
+},
+{
+  name: "Gecko Moria",
+  alias: ["moria", "gecko moria"],
+  image: moria,
+  crew: "Moria Pirates",
+  role: "Captain",
+  fruit: "Kage Kage no Mi",
+  hints: [
+    "I can manipulate shadows.",
+    "I lost my crew in the New World.",
+    "I'm a former Warlord.",
+  ],
+},
+{
+  name: "Bartholomew Kuma",
+  alias: ["kuma"],
+  image: kuma,
+  crew: "Revolutionary Army",
+  role: "Former Warlord",
+  fruit: "Nikyu Nikyu no Mi",
+  hints: [
+    "I can repel anything with my palms.",
+    "I was turned into a cyborg.",
+    "I'm a key figure in the Revolutionary Army.",
+  ],
+},
+{
+  name: "Edward Weevil",
+  alias: ["weevil"],
+  image: weevil,
+  crew: "None",
+  role: "Warlord",
+  fruit: "None",
+  hints: [
+    "I claim to be Whitebeard's son.",
+    "I'm incredibly strong.",
+    "I'm a former Warlord.",
+  ],
+},
+
+// ---- Other Pirates & Villains ----
+
+{
+  name: "Enel",
+  alias: ["enel", "eneru"],
+  image: enel,
+  crew: "Skypieans",
+  role: "God of Skypiea",
+  fruit: "Goro Goro no Mi",
+  hints: [
+    "I can control lightning.",
+    "I consider myself a god.",
+    "I'm originally from Birka.",
+  ],
+},
+{
+  name: "Corazon",
+  alias: ["Corazon", "Donxioute Rosinate", "Rosinate"],
+  image: corazon,
+  crew: "Doflamingo Pirates",
+  role: "Heart Seat Commander",
+  fruit: "Nagi Nagi no Mi",
+  hints: [
+    "I ate the Nagi Nagi no Mi",
+    "I was a double agent",
+    "I was a marine and a pirate at the same time",
+  ],
+},
+{
+  name: "Shiryu",
+  alias: ["shiryu", "shiryu of the rain"],
+  image: shiryu,
+  crew: "Blackbeard Pirates",
+  role: "Captain of the Second Ship",
+  fruit: "Suke Suke no Mi",
+  hints: [
+    "I once served in Impel Down.",
+    "I'm a master swordsman.",
+    "I can turn invisible.",
+  ],
+},
+{
+  name: "Perona",
+  alias: ["perona"],
+  image: perona,
+  crew: "Moria Pirates",
+  role: "Ghost Princess",
+  fruit: "Horo Horo no Mi",
+  hints: [
+    "I can create negative ghosts.",
+    "I lived with Mihawk for a while.",
+    "I'm known as the Ghost Princess.",
+  ],
+},
+{
+  name: "Alvida",
+  alias: ["alvida"],
+  image: alvida,
+  crew: "Buggy Pirates",
+  role: "Co-Captain",
+  fruit: "Sube Sube no Mi",
+  hints: [
+    "I used to be much larger.",
+    "My skin is now slippery.",
+    "I teamed up with Buggy.",
+  ],
+},
+{
+  name: "Foxy",
+  alias: ["foxy"],
+  image: foxy,
+  crew: "Foxy Pirates",
+  role: "Captain",
+  fruit: "Noro Noro no Mi",
+  hints: [
+    "I love games with special rules.",
+    "I can slow people with my beam.",
+    "I hosted the Davy Back Fight.",
+  ],
+},
+
+{
+  name: "Bon Clay",
+  alias: ["bon clay", "mr.2", "bentham"],
+  image: bon_clay,
+  crew: "Boroque works",
+  role: "General",
+  fruit: "Mane Mane no Mi",
+  hints: [
+    "oh come(okama) my way.",
+    "I have a special relationship with the strawhats.",
+    "I served on the crew of Mr.0.",
+  ],
+},
+
+{
+  name: "Avalo Pizarro",
+  alias: ["Pizarro"],
+  image: pizzaro,
+  crew: "Blackbeard Pirates",
+  role: "Titanic Captain",
+  fruit: "Shima shima no Mi",
+  hints: [
+    "I was imprisoned in impel down",
+    "I am one of the 10 titanic captains",
+    "I'm also known as the corrupt king",
+  ],
+},
+
+{
+  name: "Black Maria",
+  alias: ["black maria"],
+  image: black_maria,
+  crew: "Tobi Roppo",
+  role: "member",
+  fruit: "Kumo Kumo no Mi, Model: Rosamygale",
+  hints: [
+    "I'm a member of Tobbi Roppo",
+    "I have a special relationship with Kaidou",
+    "Call me spider-woman",
+  ],
+},
+
+{
+  name: "Baby 5",
+  alias: ["baby 5"],
+  image: baby_5,
+  crew: "Doflamingo Pirates",
+  role: "member",
+  fruit: "Buki Buki no Mi",
+  hints: [
+    "I ate the Buki Buki no mi",
+    "I'm inlove with Sai",
+    "Call me a weapons expert",
+  ],
+},
+
+{
+  name: "Ben Beckman",
+  alias: ["ben beckman"],
+  image: ben,
+  crew: "Read Head Pirates",
+  role: "Right hand",
+  fruit: "none",
+  hints: [
+    "I'm a member of the red haired pirates",
+    "My bounty is the second largest in the crew",
+    "I'm the most intelligent person in the East Blue",
+  ],
+},
+
+
 ];
